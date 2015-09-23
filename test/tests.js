601b6e09fb2d9686e6c6803817c696e86b713ae7
Jan Miksovsky <jan@quickui.org>
2015-09-23 14:00:43 -0700

    Unit test to confirm element's own created method is called too.

--- test/tests.js
+++ test/tests.js
@@ -1,9 +1,18 @@
 suite("MinimalComponent", function() {
 
+  setup(function() {
+    window.createdHook = null;
+  });
+
   test("behavior instantiates element template", function() {
-    var element = document.createElement('simple-element');
+    var element = document.createElement('test-template');
     assert(element.root);
     assert(element.root.textContent.trim(), "Hello");
+  });
+
+  test("component using behavior still has its own created method invoked", function(done) {
+    element = document.createElement('test-created');
+    assert(element.createdWasCalled);
   });
 
 });
