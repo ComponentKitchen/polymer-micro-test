4c07e346b10af89aa46ceef13737294b36593f1c
Jan Miksovsky <jan@quickui.org>
2015-09-23 10:23:37 -0700

    Add comments.

--- minimalComponent.js
+++ minimalComponent.js
@@ -1,10 +1,17 @@
+/*
+ * A minimal set of helper functions to go on top of polymer-micro:
+ *
+ *   1. <template> instantiation
+ *   2. Polymer-style automatic node finding
+ */
+
 (function() {
 
 // Polymer-style automatic node finding.
 // See https://www.polymer-project.org/1.0/docs/devguide/local-dom.html#node-finding.
 // This feature is not available in polymer-micro, so we provide a basic
 // version of this ourselves.
-function annotateNodesWithIds(instance) {
+function createReferencesToNodesWithIds(instance) {
   instance.$ = {};
   var nodesWithIds = instance.root.querySelectorAll('[id]');
   [].forEach.call(nodesWithIds, function(node) {
@@ -20,12 +27,15 @@ window.MinimalComponent = {
     this.root = this.createShadowRoot();
     this.root.appendChild(this.template.content.cloneNode(true));
 
+    // Create this.$.<id> properties.
+    createReferencesToNodesWithIds(this);
+
     // Initialize property values from attributes.
-    annotateNodesWithIds(this);
     this._marshalAttributes();
   },
 
   get ownerDocument() {
+    // Support both polyfilled and native HTML Imports.
     var currentScript = document._currentScript || document.currentScript;
     return currentScript.ownerDocument;
   }
