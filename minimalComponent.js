637579c1423fecb08f39bb0df744f7dd62ca35f8
Jan Miksovsky <jan@quickui.org>
2015-09-23 09:45:43 -0700

    Properties initialized through attributes

--- minimalComponent.js
+++ minimalComponent.js
@@ -1,8 +1,28 @@
+(function() {
+
+// Polymer-style automatic node finding.
+// See https://www.polymer-project.org/1.0/docs/devguide/local-dom.html#node-finding.
+// This feature is not available in polymer-micro, so we provide a basic
+// version of this ourselves.
+function annotateNodesWithIds(instance) {
+  instance.$ = {};
+  var nodesWithIds = instance.root.querySelectorAll('[id]');
+  [].forEach.call(nodesWithIds, function(node) {
+    var id = node.getAttribute('id');
+    instance.$[id] = node;
+  });
+}
+
 window.MinimalComponent = {
 
   created: function() {
+    // Instantiate template.
     this.root = this.createShadowRoot();
     this.root.appendChild(this.template.content.cloneNode(true));
+
+    // Initialize property values from attributes.
+    annotateNodesWithIds(this);
+    this._marshalAttributes();
   },
 
   get ownerDocument() {
@@ -11,3 +31,5 @@ window.MinimalComponent = {
   }
 
 };
+
+})();
